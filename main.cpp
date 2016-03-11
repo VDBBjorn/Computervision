109b765f5fa910fbf53096777fd590985e8c2658
Bjorn Vandenbussche <vandenbusschebjorn@gmail.com>
2016-03-11 10:41:38 +0100

    init main

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <cstdio>
+#include <string>
+#include "opencv2/opencv.hpp"
+#include "opencv2/highgui.hpp"
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, char** argv){
+
+	waitKey(0);
+    destroyAllWindows();
+	return 0;
+}
