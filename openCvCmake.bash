9a027d7086b018da430f02638b01efcd72c66d04
Bjorn Vandenbussche <vandenbusschebjorn@gmail.com>
2016-03-11 10:47:01 +0100

    run script

---
+++ openCvCmake.bash
@@ -0,0 +1,25 @@
+#!/bin/bash
+cd $PWD
+if [ $# != 1 ]; then
+	echo "Use: openCvCmake.bash [projectName]">&2
+	exit 1
+fi
+projectName=$1
+touch $projectName
+if [ ! -f CMakeLists.txt ]; then
+	cat > CMakeLists.txt <<EOF
+cmake_minimum_required(VERSION 2.8)
+project( $projectName )
+find_package( OpenCV REQUIRED )
+add_executable( $projectName $projectName.cpp )
+target_link_libraries( $projectName \${OpenCV_LIBS} )
+EOF
+fi;
+if [ ! -d CMake ]; then
+	mkdir CMake 1>&2 2>/dev/null
+fi;
+cd CMake
+cmake ..
+make
+cp $projectName ../
+cd ..
