b33436aa0c3521e7dde74fff9e7bd4f8e34570ca
Tim Ranson <timor393@gmail.com>
2016-04-16 15:10:13 +0200

    Update datasets

---
+++ Dataset/evaluate.py
@@ -0,0 +1,31 @@
+from numpy import *
+import matplotlib.pyplot as plt
+
+seq="01"
+
+gt=genfromtxt("%s/gtdistances.txt"%seq)[:,1]/3.6
+res=genfromtxt("%s/results.txt"%seq)[:,1]/3.6
+speeds=genfromtxt("%s/actualspeeds.txt"%seq)[:,1]
+
+distance=0
+time=0
+for i in range(len(gt)):
+	if res[i]>gt[i]:
+		print("Sorry, you crashed! Speed in frame %d exceeded safety limits."%i)
+		break
+	else:
+		distance+=speeds[i]
+		time+=speeds[i]/res[i]
+	if time>=60:
+		print("You ran out of time after %d frames!"%i)
+		break
+print("You travelled %f metres in %f seconds."%(distance,time))
+
+#plt.figure()
+#plt.plot(gt)
+#plt.plot(speeds)
+#plt.ylim([0,30])
+#plt.show()
+	
+
+
